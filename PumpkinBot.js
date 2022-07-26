eeb513aac25325891f356b981b1c153ccc12aa33
MrJeremyFisher <63616270+MrJeremyFisher@users.noreply.github.com>
2022-07-26 11:08:36 -0400

    Finish pumpkin bot

--- PumpkinBot.js
+++ PumpkinBot.js
@@ -1,14 +1,13 @@
-// Not yet completed
-
-
-const farmLength = 11; // Num of pumpkins
-const farmWidth = 9; // Total width (incl. water and stems)
-
+const farmLength = 81; // Num of pumpkins
+const farmWidth = 80; // Total width (incl. water and stems)
+const toolType = "Iron Axe"; // Name of tool to use (no underscores, capital letters)
 // No touchy ------------------------------------------
 const player = Player.getPlayer();
 const inv = Player.openInventory();
-const startZ = player.getZ();
-const startX = player.getX();
+const trueStartX = player.getX();
+const startY = player.getY();
+let startX = player.getX();
+let startZ = player.getZ();
 
 // Look at the first pumpkin
 player.lookAt(-180, 40);
@@ -16,9 +15,12 @@ player.lookAt(-180, 40);
 // Centre the player
 player.getRaw().m_6034_(Math.floor(player.getX()) + 0.5, player.getY(), Math.floor(player.getZ()) + 0.5);
 
-function lookForAxe() { // Searches the players inv for a diamond axe
+Chat.log("§a Started Farming!");
+World.playSound("block.note_block.pling", 1, 2);
+
+function lookForAxe() { // Searches the players inv for a tool
     for (i = 0; i < 35; i++) {
-        if (inv.getSlot(i).getName().getString() == "Diamond Axe") {
+        if (inv.getSlot(i).getName().getString() == toolType) {
             inv.swap(i, inv.getMap().get("hotbar")[0])
             inv.setSelectedHotbarSlotIndex(0);
         }
@@ -26,29 +28,55 @@ function lookForAxe() { // Searches the players inv for a diamond axe
 }
 
 
-// Break row going NORTH
-while (player.getZ() > startZ - farmLength - 1) {
-    if (!rev) { 
-        player.lookAt(-180, 40); 
-    } else if (rev) {
-        player.lookAt(0, 40); 
+while (player.getX() > trueStartX - farmWidth && player.getY() == startY) {
+    lookForAxe();
+    KeyBind.keyBind('key.sneak', true);
+    // Break row going NORTH
+    while (player.getZ() > startZ - farmLength - 1 && player.getY() == startY) {
+        player.lookAt(-180, 40);
+        Client.waitTick(1);
+        KeyBind.keyBind('key.forward', true);
+        KeyBind.keyBind('key.attack', true);
     }
 
-    KeyBind.keyBind('key.forward', true);
-    KeyBind.keyBind('key.attack', true);
+    KeyBind.keyBind('key.forward', false);
+    KeyBind.keyBind('key.attack', false);
+
+
+    // Shift over one row going WEST
+    startX = player.getX();
+    while (player.getX() > startX - 3 && player.getY() == startY) {
+        player.lookAt(90, 0);
+        Client.waitTick(1);
+        KeyBind.keyBind('key.forward', true);
+    }
+
+    KeyBind.keyBind('key.forward', false);
+
+    // Centre the player
+    player.getRaw().m_6034_(Math.floor(player.getX()) + 0.5, player.getY(), Math.floor(player.getZ()) + 0.5);
+
+    // Break row going SOUTH
+    while (player.getZ() < startZ && player.getY() == startY) {
+        player.lookAt(0, 40);
+        Client.waitTick(1);
+        KeyBind.keyBind('key.forward', true);
+        KeyBind.keyBind('key.attack', true);
+    }
+
+    KeyBind.keyBind('key.forward', false);
+    KeyBind.keyBind('key.attack', false);
+
+    // Shift over one row going WEST
+    startX = player.getX();
+    while (player.getX() > startX - 2 && player.getY() == startY) {
+        player.lookAt(90, 0);
+        Client.waitTick(1);
+        KeyBind.keyBind('key.forward', true);
+    }
+
+    KeyBind.keyBind('key.forward', false);
+
+    // Centre the player
+    player.getRaw().m_6034_(Math.floor(player.getX()) + 0.5, player.getY(), Math.floor(player.getZ()) + 0.5);
 }
-
-KeyBind.keyBind('key.forward', false);
-KeyBind.keyBind('key.attack', false);
-
-
-if ()
-// Shift over one row going WEST
-while (player.getX() > startX - 3) {
-    player.lookAt(90, 0);
-    KeyBind.keyBind('key.forward', true);
-}
-KeyBind.keyBind('key.forward', false);
-
-// Centre the player
-player.getRaw().m_6034_(Math.floor(player.getX()) + 0.5, player.getY(), Math.floor(player.getZ()) + 0.5);
